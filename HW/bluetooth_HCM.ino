e502879358ebaea67921c573b9514e96ffc131dd
ndharanidevi <49300764+ndharanidevi@users.noreply.github.com>
2021-05-22 09:32:12 +0530

    Update bluetooth_HCM.ino

--- HW/bluetooth_HCM.ino
+++ HW/bluetooth_HCM.ino
@@ -3,42 +3,41 @@
 #include<BleMouse.h> 
 
 
-const int MPU_addr=0x68;  // Address of the MPU-6050 using the I2C protocol
-int16_t GyY,GyZ;  //Initializing the Gyroscope values
+const int MPU_addr=0x68;  
+int16_t GyY,GyZ;  
 int vz, vy, vz_prec, vy_prec; 
 int count=0;
 
 
-BleMouse blhidm("HCM","HCM TEAM",100); //Initializing the Bluetooth Low Energy Mouse(HID) with device name, manufacturer name and initial battery level
-
+BleMouse blhidm("HCM","HCM TEAM",100); 
 
 void setup(){ 
- Serial.begin(115200);  //Serial baud rate
+ Serial.begin(115200);  
  Wire.begin();  //Wire for I2C communication
  Wire.beginTransmission(MPU_addr); //Lets commence the transmission to the gyroscope 
- Wire.write(0x6B);  // PWR_MGMT_1 register:- There is a low power mode in MPU which makes it hibernate. We are disabling that
- Wire.write(0);     // set to zero (wakes up the MPU-6050)
- Wire.endTransmission(true);  // Lets stop the transmission now so that other libraries can be initialized
- blhidm.begin();  //We have initialized the bluetooth HID mouse library
+ Wire.write(0x6B);  
+ Wire.write(0);     
+ Wire.endTransmission(true);  
+ blhidm.begin(); 
 }
 
 
 void loop(){
  Wire.beginTransmission(MPU_addr);
- Wire.write(0x43);  // starting with register 0x43 (GYRO_XOUT_L)
- Wire.endTransmission(false); //Now lets restart the transmission
- Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+ Wire.write(0x43);  
+ Wire.endTransmission(false);
+ Wire.requestFrom(MPU_addr,14,true);  
  //Hexadecimal Mumbo Jumbo coming up
- GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
- GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+ GyY=Wire.read()<<8|Wire.read();  
+ GyZ=Wire.read()<<8|Wire.read(); 
  vz = -(GyZ+100) / 200; //Cleanup
  vy = -(GyY+100 ) / 200;  //Cleanup
- Serial.print(vz);  //Just some debugging
- Serial.print(vy);  //Still some debugging
+ Serial.print(vz); 
+ Serial.print(vy);  
  blhidm.move(vz,vy);  
- if ( (vz_prec - 5) <= vz && vz <= vz_prec + 5 && (vy_prec - 5) <= vy && vy <= vy_prec + 5) //Are you really moving your head?
+ if ( (vz_prec - 5) <= vz && vz <= vz_prec + 5 && (vy_prec - 5) <= vy && vy <= vy_prec + 5) 
  {
-    count++; //To keep track of how much time your head was still
+    count++; 
     if (count == 100) 
     { 
       if (!blhidm.isPressed(MOUSE_LEFT))
